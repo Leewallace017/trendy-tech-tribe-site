a25ab2eb591ad1ccfb057ff6514a74e416d801aa
William Wallace <Leewallace017@gmail.com>
2025-11-23 18:49:26 -0800

    feat: Add new Astro pages for newsletter, EVS, about, and picks, and update package.json.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import vercel from '@astrojs/vercel';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://trendytechtribe.com',
   integrations: [mdx(), sitemap()],
+  adapter: vercel(),
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
